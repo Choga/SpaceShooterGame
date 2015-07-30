6c0057e92317b4284bc99eddafb8b9dc0f2bc790
DerekHua <DerekChristopherHua@gmail.com>
2015-07-29 21:21:29 -0400

    Enemy Spawning

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,13 +8,18 @@ public class EnemyMovement : MonoBehaviour
 	
 	private Rigidbody2D theEnemyMover;
 	
-	private Transform playerTransform; // Target Object to follow
+	private Transform playerTransform;			// Target Object to follow
 	
-	private Vector3 directionOfPlayer;
+	private Vector3 directionOfPlayer;		
 	
 	private Vector3 origRotation;
 
 	private float health;
+
+	bool challenged = false;					// Flag for enemy aggro
+
+	Vector3 worldUp = new Vector3(0f, 0f, 90f);	// World's "up" direction
+
 	
 	// Use this for initialization
 	void Start () 
@@ -22,21 +27,23 @@ public class EnemyMovement : MonoBehaviour
 		theEnemyMover = this.GetComponent<Rigidbody2D> ();
 		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 		origRotation = this.transform.eulerAngles;
-
 		health = 70;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
-		bool challenged = true;
-		
+		float distance = Vector3.Distance (this.transform.position, playerTransform.transform.position);
+
+		if (distance < 5f) 
+			challenged = true;
+		else if (distance > 10f) 
+			challenged = false;
+
+		// Actions when enemy is challenged
 		if (challenged) {
-			directionOfPlayer = playerTransform.position - this.transform.position;
-			directionOfPlayer = directionOfPlayer.normalized;    // Get Direction to Move Towards
-			
-			Vector3 worldUp = new Vector3(0f, 0f, 90f);        // World's "up" direction
-			
+			directionOfPlayer = (playerTransform.position - this.transform.position).normalized;	// Get direction to move towards			
+
 			float playerX = playerTransform.position.x;
 			float playerY = playerTransform.position.y;
 			float playerZ = playerTransform.position.z;
@@ -50,12 +57,19 @@ public class EnemyMovement : MonoBehaviour
 			
 			this.transform.Translate (directionOfPlayer * speed, Space.World); // Move enemey to player
 			this.transform.LookAt (playerTransform, worldUp);        // Points torwards Player
+		} else if (!challenged) {
+
+			float speed = 5f;
+			Vector2 vel;
+
+			vel = Random.insideUnitCircle * speed;
+			this.transform.Translate (vel * Time.deltaTime, Space.World);
 		}
 	}
 
 	public void applyDamage(float damage) {
 		health -= damage;
-		
+
 		if (health <= 0) {
 			Destroy (this.gameObject);
 			ExplosionHandler.createAndDestroyExplosion (this.gameObject.transform.position, explosion);
