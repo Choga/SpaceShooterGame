42da83b75b457a4116f1f3b84f9d95beaf032001
czhang14 <warren.zhang117@gmail.com>
2015-08-01 21:59:20 -0400

    Loot now works
    Loot spawns using Resources.load trick, which is probably just temporary
    until I can think of some more elegant solution.

--- Assets/Scripts/LootElements/LootHandler.cs
+++ Assets/Scripts/LootElements/LootHandler.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class LootHandler : MonoBehaviour {
 
-	public GameObject[] allLootTypes;
+	public static GameObject[] allLootTypes;
 	public static string[] allLootNames = {"Engine", "Guns", "Diamond", "Platinum", "PlanetCore"};
 	
 	private bool addToShip;
@@ -14,6 +14,20 @@ public class LootHandler : MonoBehaviour {
 	}
 
 	public GameObject getRandomLoot(int shipClass) {
+
+		//Initializes allLootTypes if not previously initialized
+		if (allLootTypes == null) {
+			allLootTypes = new GameObject[allLootNames.Length];
+			for(int i = 0; i < allLootNames.Length; i++) {
+				//Not necessarily the best way to do this, but (right now) get Loot GameObjects by
+				//Loading via Resources.load (which searches the Resources folder). Resources folder
+				//must be below Assets. Currently: Assets/Prefabs/Loots/Resources. Changing the location
+				//of the resources folder will require the argument of this function get changed or a 
+				//new means of loading the loot into allLootTypes.
+				allLootTypes[i] = (GameObject) Resources.Load (allLootNames[i]);
+				Debug.Log (allLootTypes[i].name);
+			}
+		}
 
 		// Getting random value for the loot spawn
 		int current = (int)(Random.Range (0, shipClass-1));
